ece2d2e00e6f937a899cb826ed4f4115c844c305
wilhelm-heiko <134288520+wilhelm-heiko@users.noreply.github.com>
2023-07-05 01:01:01 +0200

    Lost parts

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+$(document).ready(() => {
 
+    if(localStorage.getItem("students2") != null){
+       var array = JSON.parse(localStorage.getItem("students2"))
+
+        $("#studentsCount").text(array.length);
+
+    }
+    
+})
